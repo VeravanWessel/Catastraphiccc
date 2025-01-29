3880dd2935e6564c9049a131b6886e46bb593a9a
Caitjotu <caitlynjoannes@gmail.com>
2025-01-29 20:52:20 +0100

    Added hearts

---
+++ Assets/HealthHeart.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthHeart : MonoBehaviour
+{
+    public Sprite fullheart, halfheart, emptyheart;
+    Image heartImage;
+
+    private void Awake()
+    {
+        heartImage = GetComponent<Image>();
+    }
+
+    public void SetHeartImage(HeartStatus status)
+    {
+        switch (status)
+        {
+            case HeartStatus.Empty:
+                heartImage.sprite = emptyheart;
+                break;
+            case HeartStatus.Half:
+                heartImage.sprite = halfheart;
+                break;
+            case HeartStatus.Full:
+                heartImage.sprite = fullheart;
+                break;
+        }
+    }
+}
+
+public enum HeartStatus
+{
+    Empty = 0,
+    Half = 1,
+    Full = 2
+}
