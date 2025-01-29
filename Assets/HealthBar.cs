b770352a507d98731d0cd6c58f66ab86b0863050
Caitjotu <caitlynjoannes@gmail.com>
2025-01-29 20:14:12 +0100

    HealthBarStart

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public int Health = 3;
+
+    public GameObject Health3;
+    public GameObject Health2;
+    public GameObject Health1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
