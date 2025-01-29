0deff54c3671ff735dd37c1c8bc99625108dd1c5
VeravanWessel <vw.vanwessel@student.han.nl>
2025-01-29 14:58:03 +0100

    momenteelwerkthet
    momenteelwerkthet

---
+++ Assets/CharacterController2D.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 400f;
+    [SerializeField] private float movementSmoothing = 0.05f;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform groundCheck;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private Vector3 velocity = Vector3.zero;
+    private bool grounded;
+
+    public UnityEvent OnLandEvent;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        // Zorg ervoor dat het event niet null is
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+    }
+
+    private void FixedUpdate()
+    {
+        bool wasGrounded = grounded;
+        grounded = false;
+
+        // Controleer of de speler op de grond staat
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f, whatIsGround);
+        foreach (Collider2D col in colliders)
+        {
+            if (col.gameObject != gameObject)
+            {
+                grounded = true;
+                if (!wasGrounded)
+                    OnLandEvent.Invoke();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+
+        Move(move, jump);
+    }
+
+    private void Move(float move, bool jump)
+    {
+        // Beweeg de speler
+        Vector3 targetVelocity = new Vector2(move * speed, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+        // Flip als nodig
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+
+        // Jump
+        if (grounded && jump)
+        {
+            grounded = false;
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(facingRight ? 1 : -1, 1, 1);
+    }
+}
