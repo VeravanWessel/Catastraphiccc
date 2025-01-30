2613620a5067323823344abb3713bb6aa0c67ea4
VeravanWessel <vw.vanwessel@student.han.nl>
2025-01-30 13:32:24 +0100

    vermoordrat
    sad Inge

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,28 +1,30 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-
 {
-    [SerializeField] private float moveSpeed = 3f; // Snelheid van de vijand
-    [SerializeField] private float moveDistance = 5f; // Hoe ver de vijand beweegt voordat hij omkeert
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float moveDistance = 5f;
     private Vector3 startPosition;
-    private int direction = 1; // 1 = rechts, -1 = links
+    private int direction = 1;
     private bool doodgaan = false;
+    public GameObject ratdood;
+   private BoxCollider2D boxCollider;
+
+    public List<GameObject> objectenOmTeDeactiveren; // Lijst met objecten om te deactiveren
 
     private void Start()
     {
         startPosition = transform.position;
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
-        if (!doodgaan) // Alleen bewegen als doodgaan niet geactiveerd is
+        if (!doodgaan)
         {
             transform.position += new Vector3(direction * moveSpeed * Time.deltaTime, 0, 0);
 
-            // Keer om als de vijand de maximale afstand bereikt
             if (Mathf.Abs(transform.position.x - startPosition.x) >= moveDistance)
             {
                 direction *= -1;
@@ -31,19 +33,37 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    public void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (other.CompareTag("Box")) // Controleert of de vijand een object met de tag "Box" raakt
+        if (collision.gameObject.CompareTag("Box")) // Controleert of het object een "Box" is
         {
-            doodgaan = true;
-            
-          
+            float boxY = collision.transform.position.y;
+            float enemyY = transform.position.y;
+
+            if (boxY > enemyY) // Alleen doden als de doos van boven komt
+            {
+                doodgaan = true;
+                ratdood.SetActive(true);
+               
+                DeactiveerObjecten(); // Roep de functie aan om de lijst te deactiveren
+                Debug.Log("Enemy is geraakt van boven en is nu dood.");
+            }
+        }
+    }
+
+    private void DeactiveerObjecten()
+    {
+        foreach (GameObject obj in objectenOmTeDeactiveren)
+        {
+            if (obj != null)
+            {
+                obj.SetActive(false);
+            }
         }
     }
 
     private void Flip()
     {
-        // Draai het object horizontaal om
         Vector3 newScale = transform.localScale;
         newScale.x *= -1;
         transform.localScale = newScale;
