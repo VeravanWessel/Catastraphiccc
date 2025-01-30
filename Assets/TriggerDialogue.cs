0380486050c8761ace038b0b0a420699dd362c51
VeravanWessel <vw.vanwessel@student.han.nl>
2025-01-30 14:56:29 +0100

    Hij doet het
    Yeeaah

--- Assets/TriggerDialogue.cs
+++ Assets/TriggerDialogue.cs
@@ -1,34 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class TriggerDialogue : MonoBehaviour
 {
+    // Events die worden geactiveerd wanneer de speler het triggergebied binnenkomt of verlaat
     public UnityEvent onTriggerEnter = new UnityEvent();
     public UnityEvent onTriggerExit = new UnityEvent();
 
-    void OnTriggerEnter(Collider other)
+    // Wanneer de speler het triggergebied binnenkomt
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player")) onTriggerEnter.Invoke();
-        Debug.Log("Enter");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            onTriggerEnter.Invoke();  // Activeer het event bij binnenkomst
+            Debug.Log("Enter");
+        }
     }
 
-    void OnTriggerExit(Collider other)
+    // Wanneer de speler het triggergebied verlaat
+    void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player")) onTriggerExit.Invoke();
-        Debug.Log("Exit");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            onTriggerExit.Invoke();  // Activeer het event bij verlaten
+            Debug.Log("Exit");
+        }
     }
-
-      // void OnCollision2DEnter(Collider2D Collision2D)
-    // {
-    //     if (Collision2D.gameObject.CompareTag("Player")) onCollision2DEnter.Invoke();
-    //     Debug.Log("Enter");
-    // }
-
-    // void OnCollision2DExit(Collider2D Collision2D)
-    // {
-    //     if (Collision2D.gameObject.CompareTag("Player")) onCollision2DExit.Invoke();
-    //     Debug.Log("Exit");
-    // }
 }
